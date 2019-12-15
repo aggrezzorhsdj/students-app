0a6ede0b19572f7c814b4afe9d3ba0b92f3322ee
aggrezzorhsdj <aggrezzorhsdj@gmail.com>
2019-12-15 22:33:47 +0300

    update with fixes

--- src/app/add-form/add-form.component.ts
+++ src/app/add-form/add-form.component.ts
@@ -14,18 +14,18 @@ import { IStudents } from '../students';
 export class AddFormComponent implements OnInit {
   students = STUDENTS;
   addForm: FormGroup;
+  index: number;
+  editForm = false;
+  submitted = false;
   constructor(private formBuilder: FormBuilder) {
-    this.addForm = this.formBuilder.group({
-      userFullName: this.formBuilder.group({
-        userFirstName: ['', Validators.required],
-        userLastName: ['', Validators.required],
-      }, {validator: this.userNameValidator}),
-      userGroupName: ['', Validators.required],
-      userAge: ['', Validators.required],
-      userAvgMark: ['', Validators.required],
-      userBirthDay: ['', [Validators.required, this.userBirthDateValidator]],
-      userFlage: false,
-    });
+  }
+  @Input()
+  set indexElement(index: number) {
+    this.index = index ? index : 0;
+  }
+  @Input()
+  setForm(b: boolean) {
+    this.editForm = b;
   }
   public userNameValidator(group: FormGroup) {
     const userFirstName = group.controls.userFirstName.value;
@@ -54,27 +54,55 @@ export class AddFormComponent implements OnInit {
     const currentDate = new Date();
     return currentDate.toISOString().substring(0, 10);
   }
+  get f() { return this.addForm.controls; }
   public submitForm(e: Event) {
+    this.submitted = true;
+    console.log(this.submitted);
     e.preventDefault();
+    let added: IStudents;
     if (this.addForm.valid) {
-      const added: IStudents = {
+      const year: Date = new Date();
+      let curDate: Date = new Date(this.addForm.get('userBirthDay').value);
+      added = {
         id: this.students.length + 1,
         name: this.addForm.get('userFullName').get('userFirstName').value,
         surname: this.addForm.get('userFullName').get('userLastName').value,
-        age: this.addForm.get('userAge').value,
+        age: year.getFullYear(),
         avgmark: this.addForm.get('userAvgMark').value,
         groupname: this.addForm.get('userGroupName').value,
-        birthday: this.addForm.get('userBirthDay').value,
+        birthday: year.getFullYear() - curDate.getFullYear(),
         flage: false,
       };
-      this.addStudents(added);
-
+      switch (this.editForm) {
+        case false:
+          this.addStudents(added);
+          break;
+        case true:
+          this.editStudents(added, this.index);
+          break;
+      }
     }
   }
   public addStudents(students: IStudents): void {
     this.students.push(students);
   }
+  public editStudents(students: IStudents, index: number): void {
+    this.students[index] = students;
+  }
+  get userFirstName() {
+    return this.addForm.get('userFirstName');
+  }
   ngOnInit() {
+    this.addForm = this.formBuilder.group({
+      userFullName: this.formBuilder.group({
+        userFirstName: ['', Validators.required],
+        userLastName: ['', Validators.required],
+      }, {validator: this.userNameValidator}),
+      userGroupName: ['', Validators.required],
+      userAvgMark: ['', Validators.required],
+      userBirthDay: ['', [Validators.required, this.userBirthDateValidator]],
+      userFlage: false,
+    });
   }
 
 }
